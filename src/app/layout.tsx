1c353b9f96bbbefd9018b651751c595fe5d40b90
hussamallah <hus.1998.apr@gmail.com>
2025-10-20 12:42:51 -0400

    Add Vercel Analytics component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Tajawal } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const tajawal = Tajawal({
@@ -28,6 +29,7 @@ export default function RootLayout({
         }}
       >
         {children}
+        <Analytics />
         <script async src="//www.instagram.com/embed.js"></script>
       </body>
     </html>
